a3fa0c6dde2977c8b4bcf32fc07679bd6f1600c8
XingLuoXL <1051485523@qq.com>
2023-03-31 22:31:07 +0800

    firstmain

---
+++ filesysytemtest/main.cpp
@@ -0,0 +1,29 @@
+#include"file.h"
+
+
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+	menu();
+	int select;
+	cin >> select;
+	switch(select)
+	{
+	case 1:
+		function_01(); 
+		break;
+	case 2:break;
+	case 3:break;
+	case 4:break;
+	case 5:break;
+	case 6:break;
+	case 7:break;
+	case 8:break;
+	default:
+		break;
+	}
+
+}
