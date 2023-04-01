5689c56cda24e74d29597589edc18a36d8b36b4c
XingLuoXL <1051485523@qq.com>
2023-04-01 13:37:04 +0800

    实现功能2，用户输入已存在标签的3中处理办法

--- filesysytemtest/1234.cpp
+++ filesysytemtest/1234.cpp
@@ -5,7 +5,20 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-
+bool function_01(address filepath)
+{
+	fs::path str(filepath);
+	if (!fs::exists(str))		//必须先检测目录是否存在才能使用文件入口.
+		return 1;
+	fs::directory_entry entry(str);		//文件入口
+	if (entry.status().type() == fs::file_type::directory)	//这里用了C++11的强枚举类型
+		cout << "该路径是一个目录" << endl;
+	fs::directory_iterator list(str);	        //文件入口容器
+	for (auto& it : list)
+		cout << it.path().filename() << endl;	//通过文件入口（it）获取path对象，再得到path对象的文件名，将之输出
+	system("pause");
+	return 0;
+}
 
 void function_02(std::map < tag, std::vector<file> >&FILE)
 {
@@ -18,8 +31,7 @@ void function_02(std::map < tag, std::vector<file> >&FILE)
 		if (getout())break;
 		string tipname;
 		string filename;
-		tag tip;
-		std::vector<file> document;
+		tag tip;     
 		cout << "分别输入文件名和标签名，用空格隔开" << endl;
 		cin >> filename;
 		cin >> tipname;
@@ -27,6 +39,7 @@ void function_02(std::map < tag, std::vector<file> >&FILE)
 		file newfile(filename, fileaddress + "//" + filename);
 		if (FILE.find(tip) == FILE.end())  //标签名不存在，添加标签操作合法，则存入map中
 		{
+			std::vector<file> document;    //新的vector<file>储存新文件
 			document.push_back(newfile);
 			FILE.insert(make_pair(tip, document));
 			cout << "添加成功" << endl;
@@ -34,20 +47,32 @@ void function_02(std::map < tag, std::vector<file> >&FILE)
 		else			//如果标签已存在，由用户选择使用其他标签名还是给标签添加说明
 		{
 			cout << "已存在 " << tip.name << " 标签" << endl;
-			cout << "如需要使用其他标签请输入1，如需要给标签添加说明请输入2" << endl;
+			cout << "如需要使用其他标签请输入1，如需要给该标签添加说明请输入2，如直接添加该标签请输入3" << endl;
+			tag old_tip;
+			old_tip.name = tip.name;			//如果用户需要修改标签说明，删除旧标签，将原value存至此标签对应处
+			std::vector<file>*tempfile;							 //如果用户需要修改标签说明，临时储存value值
+			std::vector<file> document;							//如果用户需要修改标签说明，创建新的vector<file>储存新文件
 			int select;
 			cin >> select;
-			if (select == 1)continue;
-			else if (select == 2)//先给已存在的标签修改说明，再给新增加的标签修改说明
+			switch (select)
 			{
-				std::vector<file>newfile;
-				newfile = FILE[tip];
-				FILE.erase(FILE.find(tip));
-
-			}
-			else
-			{
-				cout << "没有这个选项！！！";
+			case 1: continue;
+			case 2:
+					tempfile = &FILE[tip];
+					FILE.erase(FILE.find(tip));
+					cout << "请输入对原标签的说明" << endl;
+					cin >> old_tip.explain;
+					cout << "请输入对新标签的说明" << endl;
+					cin >> tip.explain;
+					document.push_back(newfile);
+					FILE.insert(make_pair(old_tip, *tempfile));
+					FILE.insert(make_pair(tip, document));
+					continue;
+			case 3:
+				FILE[tip].push_back(newfile);
+				continue;
+			default:
+				cout << "没有这个选项！！！" << endl;
 				continue;
 			}
 		}
