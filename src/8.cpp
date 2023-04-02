e1d48d76cf56ba50639f3690d7db1b3bb94159bf
llb <2718650151@qq.com>
2023-04-02 17:34:03 +0800

    fix function8

--- src/8.cpp
+++ src/8.cpp
@@ -15,76 +15,71 @@ void function_08(std::map < tag, std::vector<file> > &FILE) {
 	// std::cin>>shuoming;
     tip.name = labal;
 	// tip.explain=shuoming;
+	std::map<tag, std::vector<file>>::iterator it;
+	for(it=FILE.begin();it!=FILE.end();it++){
+		if(it->first.name==labal){
+			std::cout<<"当前标签的简介是"<<it->first.explain<<std::endl;
+			std::vector<file>result = it->second;
+			for (int i = 0; i < result.size(); i++) {
+				std::cout << result[i].getname() << std::endl;
+			}
+			std::cout<<"是否有你需要改名的文件，无请输入1，有则输入其他数字"<<std::endl;
+			int op;
+			std::cin>>op;
+			if(op==1)continue;
+			else{
+				int x=-1;
 
-	if (FILE.find(tip) != FILE.end()) {
-		auto it = FILE.find(tip);
-		std::vector<file>result = it->second;
+				std::cout << "请输入旧文件的名称" << std::endl;
+				std::cin >> old_name;
+
+				for (int i = 0; i < result.size(); i++) {
+					if(result[i].getname()==old_name)x=i;
+				}
+
+				if(x==-1){
+					std::cout<<"没有找到相应文件"<<std::endl;
+					return;
+				}
+
+				old_name=result[x].getip();
+				file oldfile=result[x];
+
+				fs::path father(old_name);
+				
+				old_name=(father.parent_path().u8string());
+				
+				std::cout << "请输入新文件的名称" << std::endl;
+				std::cin >> new_name;
+				
+				old_name=old_name+"//"+new_name;
+
+				file newfile(new_name,old_name);
+
+				new_name=old_name;
+
+				old_name=result[x].getip();
+
+				for(auto it:FILE){
+					for(int i=0;i<it.second.size();i++){
+						
+						if((it.second[i].getip()==oldfile.getip())&&(it.second[i].getname()==oldfile.getname())){
+							
+							FILE[it.first].erase(FILE[it.first].begin()+i);
+							FILE[it.first].push_back(newfile);
+							break;
+						}
+					}
+				}//遍历所有标签寻找是否有需要被改名的文件
 		
-		
-		for (int i = 0; i < result.size(); i++) {
-			std::cout << result[i].getname() << std::endl;
-		}
-
-		int x=-1;
-
-		std::cout << "请输入旧文件的名称" << std::endl;
-		std::cin >> old_name;
-
-		for (int i = 0; i < result.size(); i++) {
-			if(result[i].getname()==old_name)x=i;
-		}
-
-		if(x==-1){
-			std::cout<<"没有找到相应文件"<<std::endl;
-			return;
-		}
-
-		old_name=result[x].getip();
-		file oldfile=result[x];
-
-		fs::path father(old_name);
-		
-		old_name=(father.parent_path().u8string());
-		
-		std::cout << "请输入新文件的名称" << std::endl;
-		std::cin >> new_name;
-		
-		old_name=old_name+"//"+new_name;
-
-		file newfile(new_name,old_name);
-
-		new_name=old_name;
-
-		old_name=result[x].getip();
-
-		for(auto it:FILE){
-			for(int i=0;i<it.second.size();i++){
-				// std::cout<<it.second[i].getip()<<" "<<oldfile.getip()<<std::endl;
-				// std::cout<<it.second[i].getname()<<" "<<oldfile.getname()<<std::endl;
-				if((it.second[i].getip()==oldfile.getip())&&(it.second[i].getname()==oldfile.getname())){
-					// std::cout<<1<<std::endl;
-					FILE[it.first].erase(FILE[it.first].begin()+i);
-					FILE[it.first].push_back(newfile);
-					break;
+				if (!rename(old_name.c_str(), new_name.c_str())){
+					std::cout<<"rename success"<<std::endl;	
+				}
+				else {
+					std::cout<<"rename error"<<std::endl;
 				}
 			}
-		}//遍历所有标签寻找是否有需要被改名的文件
-
-		// FILE[tip].erase(FILE[tip].begin()+x);
-		// FILE[tip].push_back(newfile);//删除旧文件增加新文件
-
-		
-		if (!rename(old_name.c_str(), new_name.c_str())){
-			std::cout<<"rename success"<<std::endl;	
-		}
-		else {
-			std::cout<<"rename error"<<std::endl;
 		}
 	}
-	else {
-		std::cout<<"不存在该标签"<<std::endl;
-    }
-	
-	
 	return;
 }
