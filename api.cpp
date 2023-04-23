fd37c2d6d9c9638f16274f47ef12d53d6914d8cc
XingLuoXL <1051485523@qq.com>
2023-04-23 22:18:05 +0800

    firstone

---
+++ api.cpp
@@ -0,0 +1,50 @@
+
+#include "api.h"
+#include <filesystem>
+namespace fs = std::filesystem;
+
+// 用于储存，增删用函数实现以和 View 同步
+std::set<File> FileList;
+std::vector<Tag> TagList;
+std::set<int>TagScript;
+
+std::vector<Tag*> fileshowtag(const File&)
+{
+
+}  //展示单文件标签
+bool fileindir(File file)
+{
+
+}  //确认输入文件存在于目录下
+Tag* taginvec(string name, string explain)
+{
+
+}  //确认输入tag存在于vector中
+File* fileinvec(string name, string addr)
+{
+
+}  //确认输入文件存在于vector中，用于给文件加标签
+File* fileinvec(string name)
+{
+
+}  //确认输入文件存在于vector中，用于删标签
+bool fileaddtag(File* file, Tag* tag)
+{
+
+}  //给文件添加标签
+bool filedeltag(File* file, Tag* tagpoint)
+{
+
+}  //给文件删除标签
+bool tagdel(string name, string explain)
+{
+
+}  //删除某标签
+bool tagrename(string old_name, string new_name, string explain)
+{
+
+} //标签改名
+bool tagexplain(string name, string explain)
+{
+
+}  //给标签添加说明
---
+++ api.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include<filesystem>
+#include<iostream>
+#include<vector>
+#include<set>
+
+using std::string;
+
+typedef string site;
+
+class File;
+struct Tag;
+
+struct File {
+    std::string address;
+    std::string name;
+};
+
+struct Tag {
+    std::string name;
+    std::string explain = "";
+    std::vector<File*>T_filelist;
+    bool operator==(const Tag &T){
+        return (this->name == T.name)&&(this->explain == T.explain)&&(this ->T_filelist == T.T_filelist);
+    }
+};
+
+std::vector<Tag*> fileshowtag(const File&);  //展示单文件标签
+bool fileindir(File file);  //确认输入文件存在于目录下
+Tag* taginvec(string name, string explain);  //确认输入tag存在于vector中
+File* fileinvec(string name, string addr);  //确认输入文件存在于vector中，用于给文件加标签
+File* fileinvec(string name);  //确认输入文件存在于vector中，用于删标签
+bool fileaddtag(File* file, Tag* tag);  //给文件添加标签
+bool filedeltag(File* file, Tag* tagpoint);  //给文件删除标签
+bool tagdel(string name, string explain);  //删除某标签
+bool tagrename(string old_name, string new_name, string explain);  //标签改名
+bool tagexplain(string name, string explain);  //给标签添加说明
+
+
