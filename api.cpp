2c17f50f80b566ea059dc722260045fcc2b4e7dc
XingLuoXL <1051485523@qq.com>
2023-05-21 15:46:07 +0800

    Wan finish first

--- api.cpp
+++ api.cpp
@@ -6,44 +6,93 @@ namespace fs = std::filesystem;
 // 用于储存，增删用函数实现以和 View 同步
 std::set<File> FileList;
 std::vector<Tag> TagList;
-std::set<int>TagScript;
+std::queue<int>TagScript;
 
 std::vector<Tag*> fileshowtag(const File&)
 {
 
 }  //展示单文件标签
+
 bool fileindir(File file)
 {
 
 }  //确认输入文件存在于目录下
-Tag* taginvec(string name, string explain)
-{
 
-}  //确认输入tag存在于vector中
+
+Tagint taginvec(string name, string explain)
+{
+    //先查找，存在tag则返回其下标
+	for(int i = 0; i < TagList.size(); ++i)
+	{
+		if(TagList[i].name==name && TagList[i].explain==explain) return i;
+	}
+    //不存在tag则构建一个
+    Tag newtag(name,explain);
+    if(!TagScript.empty()){
+        TagList[TagScript.front()] = newtag;
+        TagScript.pop();
+    }
+    else{
+        TagList.push_back(newtag);
+    }
+    return TagList.size();
+}  //确认输入tag存在于vector中,存在则返回其下标，不存在则创建并返回新的size
+
+File* fileinset(string name, string addr)
+{
+    File newfile(name,addr);
+    FileList.insert(newfile);
+    auto itr = FileList.find(newfile);
+    File* it = &(File)*itr;
+    return it;
+    
+
+}  //确认输入文件存在于set中，用于给文件加标签
+
 File* fileinvec(string name, string addr)
 {
-
-}  //确认输入文件存在于vector中，用于给文件加标签
-File* fileinvec(string name)
-{
-
+    for(int i = 0; i < TagList.size(); ++i)
+	{
+		for(int j = 0; j < TagList[i].T_filelist.size();++j)
+        {
+            if(TagList[i].T_filelist[j]->name==name&&TagList[i].T_filelist[j]->address==addr)return TagList[i].T_filelist[j];
+        }
+        return NULL;
+	}
 }  //确认输入文件存在于vector中，用于删标签
+
+
+
+
 bool fileaddtag(File* file, Tag* tag)
 {
 
 }  //给文件添加标签
-bool filedeltag(File* file, Tag* tagpoint)
-{
 
+bool filedeltag(File* file, Tagint tagpoint)
+{
+    for(int i = 0; i < TagList[tagpoint].T_filelist.size(); i++)
+    {
+        //如果此标签里有此文件，删除并返回1，否则返回0
+        if(TagList[tagpoint].T_filelist[i]->name == file->name && TagList[tagpoint].T_filelist[i]->address == file->address)
+        {
+            TagList[tagpoint].T_filelist.erase(TagList[tagpoint].T_filelist.begin() + i);
+            return 1;
+        }
+    }
+    return 0;
 }  //给文件删除标签
+
 bool tagdel(string name, string explain)
 {
 
 }  //删除某标签
-bool tagrename(string old_name, string new_name, string explain)
-{
 
+bool tagrename(Tagint delete_tagpoint, string new_name)
+{
+    TagList[delete_tagpoint].name = new_name;
 } //标签改名
+
 bool tagexplain(string name, string explain)
 {
 
--- api.h
+++ api.h
@@ -3,11 +3,13 @@
 #include<filesystem>
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<set>
 
 using std::string;
 
 typedef string site;
+typedef int Tagint;
 
 class File;
 struct Tag;
@@ -15,11 +17,24 @@ struct Tag;
 struct File {
     std::string address;
     std::string name;
+    File(std::string address,std::string name)
+    {
+        this->name=name;
+        this->address=address;
+    }
+    bool operator<(const struct File & f)const       //重载<运算符
+    {                                                //根据name和address去重
+            return this->name == f.name && this->address == f.address;
+    }
 };
-
 struct Tag {
     std::string name;
     std::string explain = "";
+    Tag(std::string name,std::string explain)
+    {
+        this->name=name;
+        this->explain=explain;
+    }
     std::vector<File*>T_filelist;
     bool operator==(const Tag &T){
         return (this->name == T.name)&&(this->explain == T.explain)&&(this ->T_filelist == T.T_filelist);
@@ -28,8 +43,8 @@ struct Tag {
 
 std::vector<Tag*> fileshowtag(const File&);  //展示单文件标签
 bool fileindir(File file);  //确认输入文件存在于目录下
-Tag* taginvec(string name, string explain);  //确认输入tag存在于vector中
-File* fileinvec(string name, string addr);  //确认输入文件存在于vector中，用于给文件加标签
+Tagint taginvec(string name, string explain);  //确认输入tag存在于vector中
+File* fileinset(string name, string addr);  //确认输入文件存在于vector中，用于给文件加标签
 File* fileinvec(string name);  //确认输入文件存在于vector中，用于删标签
 bool fileaddtag(File* file, Tag* tag);  //给文件添加标签
 bool filedeltag(File* file, Tag* tagpoint);  //给文件删除标签
